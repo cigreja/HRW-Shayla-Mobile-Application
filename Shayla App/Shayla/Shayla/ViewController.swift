faf9c4ba5038065c64473190516725b0377eccad
Chintan Vaghela <cheaterchintz@gmail.com>
2017-03-26 21:34:53 -0400

    FbLogin
    Integrated FbLogin. Will add sign-in logins and  parse / save the user
    data later when we have database integrated.

--- Shayla App/Shayla/Shayla/ViewController.swift
+++ Shayla App/Shayla/Shayla/ViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var facebbookLoginButton: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fbLoginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
+        fbLoginButton.center = self.view.center
+        facebbookLoginButton.insertArrangedSubview(fbLoginButton, at: 0)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 

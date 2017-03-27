47f148b3dd97f71506d701f4880e060e0e7d7e9a
Chintan Vaghela <cheaterchintz@gmail.com>
2017-03-27 00:12:07 -0400

    FB Data Access
    Successfully retrieving user’s data when user logs in via facebook.

--- Shayla App/Shayla/Shayla/ViewController.swift
+++ Shayla App/Shayla/Shayla/ViewController.swift
@@ -7,20 +7,57 @@
 //
 
 import UIKit
-import FacebookCore
 import FacebookLogin
+import FBSDKCoreKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
 
     @IBOutlet weak var facebbookLoginButton: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        showFacebookButton()
+    }
+    
+    // Facebook Button
+    func showFacebookButton() {
+        
         let fbLoginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
         fbLoginButton.center = self.view.center
+        fbLoginButton.delegate = self
         facebbookLoginButton.insertArrangedSubview(fbLoginButton, at: 0)
+    }
+    
+    // Facebook Login
+    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
         
+        switch result {
+            
+        case .failed(let error):
+            print(error)
+            
+        case .cancelled:
+            print("User cancelled login.")
+            
+        case .success:
+            
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name,last_name, picture.type(large),email,updated_time"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil) {
+                    
+                    if let userDict = result as? NSDictionary {
+                        
+                        print(userDict)
+                    }
+                }
+            })
+        }
+    }
+    
+    // Facebook Logout
+    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        
+        print("User Logged Out")
     }
 }
 
